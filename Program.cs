636ff8f985646c3c26fe20937e6efb23184ef4af
Duy Nguyen <duy.nguyen@alumni.pitt.edu>
2023-04-20 09:27:56 -0400

    add working console app

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Xml.Linq;
+
+namespace AnkiAppXmlGenerator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // CSV file to read from.
+        // Column 1 is the English word
+        // Column 2 is the Vietnamese word
+        string inputFilename = "new-vocab.csv";
+        string filepath = "files";
+        string deckName = "vocab11";
+        string outputFileName = $"{deckName}.xml";
+
+        Console.WriteLine($"Reading File: '{inputFilename}'\n");
+
+        string[] source = File.ReadAllLines(Path.Combine(filepath, inputFilename));
+
+        XElement flashDeck = new XElement("deck", new XAttribute("name", deckName),
+            new XElement("fields",
+                new XElement("text", "", new XAttribute("name", "Front"), new XAttribute("sides", "11")),
+                new XElement("tts", "", new XAttribute("name", "Back"), new XAttribute("sides", "01"), new XAttribute("lang", "vi-VN"))
+            ),
+            new XElement("cards",
+                from str in source
+                let fields = str.Split(',')
+                select new XElement("card",
+                        new XElement("field", new XAttribute("name", "Back"),
+                            new XElement("tts", fields[1])
+                        ),
+                        new XElement("field", fields[0], new XAttribute("name", "Front")))
+            )
+        );
+
+        Console.WriteLine(flashDeck);
+
+        // Write xml string to a new file.
+        using (StreamWriter outputFile = new StreamWriter(Path.Combine(filepath, outputFileName)))
+        {
+            outputFile.WriteLine(flashDeck);
+        }
+    }
+}
